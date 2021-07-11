b593cf4dad21171e30c256589c9258320f5adda5
Rhalf Wendel David Caacbay <rhalfcaacbay@gmail.com>
2021-07-11 15:04:26 +0800

    20210711
    -updated build

--- source code/basics/gsm/gsm.ino
+++ source code/basics/gsm/gsm.ino
@@ -2,21 +2,21 @@
 
 uint8_t RX = 8, TX = 7;
 
-SoftwareSerial bluetooth(RX, TX);
+SoftwareSerial gsm(RX, TX);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  bluetooth.begin(9600);
+  gsm.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (bluetooth.available()) {
-    Serial.write(bluetooth.read());
+  if (gsm.available()) {
+    Serial.write(gsm.read());
   }
   if (Serial.available()) {
-    bluetooth.write(Serial.read());
+    gsm.write(Serial.read());
   }
 }
